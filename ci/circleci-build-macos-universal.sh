11accff133e89c19113a871a1a8f6eeadd2ccf2d
Dave <bdbcat@yahoo.com>
2023-12-11 11:58:28 -0500

    Adapt for MacOS universal, #11

--- ci/circleci-build-macos-universal.sh
+++ ci/circleci-build-macos-universal.sh
@@ -77,16 +77,16 @@ make install
 make package
 
 # Install cloudsmith needed by upload script
-python3 -m pip install -q --user cloudsmith-cli
+#python3 -m pip install -q --user cloudsmith-cli
 
 # Required by git-push
-python3 -m pip install -q --user cryptography
+#python3 -m pip install -q --user cryptography
 
 # python3 installs in odd place not on PATH, teach upload.sh to use it:
-pyvers=$(python3 --version | awk '{ print $2 }')
-pyvers=$(echo $pyvers | sed -E 's/[\.][0-9]+$//')    # drop last .z in x.y.z
-py_dir=$(ls -d  /Users/*/Library/Python/$pyvers/bin)
-echo "export PATH=\$PATH:$py_dir" >> ~/.uploadrc
+#pyvers=$(python3 --version | awk '{ print $2 }')
+#pyvers=$(echo $pyvers | sed -E 's/[\.][0-9]+$//')    # drop last .z in x.y.z
+#py_dir=$(ls -d  /Users/*/Library/Python/$pyvers/bin)
+#echo "export PATH=\$PATH:$py_dir" >> ~/.uploadrc
 
 # Create the cached /usr/local archive
 if [ -n "$CI"  ]; then
